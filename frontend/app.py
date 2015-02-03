b8ec926324a55092632b1b71dc56328d181396fa
Epheo <root@epheo.eu>
2015-02-03 22:29:47 +0000

    front

--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,6 @@ import vincent
 from flask import Flask, render_template
 app = Flask(__name__)
 
-app.run(host='0.0.0.0')
-
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -24,4 +22,4 @@ def stocks():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
